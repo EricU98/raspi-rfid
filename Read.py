7594b33087fe3d50af59d9d9fe0539e865d7d8f3
EricU98 <eric.uteg@web.de>
2024-08-15 08:53:11 +0200

    changed

--- Read.py
+++ Read.py
@@ -1,25 +1,69 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
 import RPi.GPIO as GPIO
-from mfrc522 import SimpleMFRC522
-import pyautogui
-import time
+import MFRC522
+import signal
 
-reader = SimpleMFRC522()
+continue_reading = True
 
-def convert_uid_to_decimal(uid_bytes):
-    # UID ist eine Liste von vier Bytes: [Byte1, Byte2, Byte3, Byte4]
-    decimal_id = (uid_bytes[0] << 24) + (uid_bytes[1] << 16) + (uid_bytes[2] << 8) + uid_bytes[3]
+# Capture SIGINT for cleanup when the script is aborted
+def end_read(signal,frame):
+    global continue_reading
+    print("Ctrl+C captured, ending read.")
+    continue_reading = False
+    GPIO.cleanup()
+
+# Hook the SIGINT
+signal.signal(signal.SIGINT, end_read)
+
+# Create an object of the class MFRC522
+MIFAREReader = MFRC522.MFRC522()
+
+# Welcome message
+print("Welcome to the MFRC522 data read example")
+print("Press Ctrl-C to stop.")
+
+def convert_uid_to_decimal(uid):
+    # Kombinieren Sie die vier Bytes in eine 32-Bit-Zahl
+    decimal_id = (uid[0] << 24) + (uid[1] << 16) + (uid[2] << 8) + uid[3]
     return decimal_id
 
-try:
-    while True:
-        print("Warte auf Tag...")
-        id, text = reader.read()
-        # Extrahiere die UID-Bytes manuell
-        uid_bytes = [147, 126, 218, 155]  # Ersetzen Sie dies durch Ihre UID-Byte-Werte
-        decimal_id = convert_uid_to_decimal(uid_bytes)
-        print("Gelesen: ID: %s" % decimal_id)
-        pyautogui.write(str(decimal_id))
-        pyautogui.press('enter')
-        time.sleep(1)
-finally:
-    GPIO.cleanup()
+# This loop keeps checking for chips. If one is near it will get the UID and authenticate
+while continue_reading:
+    
+    # Scan for cards    
+    (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+
+    # If a card is found
+    if status == MIFAREReader.MI_OK:
+        print("Card detected")
+    
+    # Get the UID of the card
+    (status,uid) = MIFAREReader.MFRC522_Anticoll()
+
+    # If we have the UID, continue
+    if status == MIFAREReader.MI_OK:
+
+        # Print UID
+        print("Card read UID: %s,%s,%s,%s" % (uid[0], uid[1], uid[2], uid[3]))
+        
+        # Konvertiere die UID in eine Dezimalzahl
+        decimal_id = convert_uid_to_decimal(uid)
+        print("Converted ID: %s" % decimal_id)
+    
+        # This is the default key for authentication
+        key = [0xFF,0xFF,0xFF,0xFF,0xFF,0xFF]
+        
+        # Select the scanned tag
+        MIFAREReader.MFRC522_SelectTag(uid)
+
+        # Authenticate
+        status = MIFAREReader.MFRC522_Auth(MIFAREReader.PICC_AUTHENT1A, 8, key, uid)
+
+        # Check if authenticated
+        if status == MIFAREReader.MI_OK:
+            MIFAREReader.MFRC522_Read(8)
+            MIFAREReader.MFRC522_StopCrypto1()
+        else:
+            print("Authentication error")
