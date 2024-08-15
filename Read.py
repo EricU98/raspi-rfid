7b08b1022e3f9d386ed756bf50ad17343858fbb9
EricU98 <eric.uteg@web.de>
2024-08-15 08:59:15 +0200

    test

--- Read.py
+++ Read.py
@@ -1,14 +1,11 @@
-#!/usr/bin/env python
-# -*- coding: utf8 -*-
-
 import RPi.GPIO as GPIO
-import MFRC522
+from MFRC522 import MFRC522
 import signal
 
 continue_reading = True
 
 # Capture SIGINT for cleanup when the script is aborted
-def end_read(signal,frame):
+def end_read(signal, frame):
     global continue_reading
     print("Ctrl+C captured, ending read.")
     continue_reading = False
@@ -18,7 +15,7 @@ def end_read(signal,frame):
 signal.signal(signal.SIGINT, end_read)
 
 # Create an object of the class MFRC522
-MIFAREReader = MFRC522.MFRC522()
+MIFAREReader = MFRC522()
 
 # Welcome message
 print("Welcome to the MFRC522 data read example")
@@ -33,14 +30,14 @@ def convert_uid_to_decimal(uid):
 while continue_reading:
     
     # Scan for cards    
-    (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+    (status, TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
 
     # If a card is found
     if status == MIFAREReader.MI_OK:
         print("Card detected")
     
     # Get the UID of the card
-    (status,uid) = MIFAREReader.MFRC522_Anticoll()
+    (status, uid) = MIFAREReader.MFRC522_Anticoll()
 
     # If we have the UID, continue
     if status == MIFAREReader.MI_OK:
@@ -53,7 +50,7 @@ while continue_reading:
         print("Converted ID: %s" % decimal_id)
     
         # This is the default key for authentication
-        key = [0xFF,0xFF,0xFF,0xFF,0xFF,0xFF]
+        key = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]
         
         # Select the scanned tag
         MIFAREReader.MFRC522_SelectTag(uid)
